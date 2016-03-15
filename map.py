da1dfea9704ab320a206b100d531e035c9025b93
Shahin Rostami <srostami@bournemouth.ac.uk>
2016-03-15 18:47:03 +0000

    new comments/docstring

--- map.py
+++ map.py
@@ -16,6 +16,10 @@ def beep():
 
 
 def spawn_coin(map):
+    """
+    Randomly selects a map location until one is found which isn't currently
+    a wall (1) or the player (x). Once it finds one, it spawns a coin there.
+    """
     y = randint(0, len(map) - 1)
     x = randint(0, y)
     while((map[y][x] == "1") or (map[y][x] == "X")):
@@ -61,6 +65,7 @@ def controller(key_input, map):
     move_x = 0
     move_y = 0
 
+    # keyboard input handled here. WASD to move, Q to quit.
     if(key_input == "q"):
         exit()
     if(key_input == "s"):
@@ -72,9 +77,12 @@ def controller(key_input, map):
     if(key_input == "d"):
         move_y = 1
 
+    # if the player attempted to move into a wall, play a beep
     if(map[player_x + move_x][player_y + move_y] == "1"):
         beep()
+    # otherwise, move the player
     else:
+        # if the player moved into a coin, collect it and spawn a new one
         if(map[player_x + move_x][player_y + move_y] == "C"):
             map = collect_coin(map, player_x + move_x, player_y + move_y)
             map = spawn_coin(map)
@@ -83,13 +91,16 @@ def controller(key_input, map):
 
     return map
 
+# load the initial map and spawn the first coin
 map = load_map("map_{}.txt".format(level))
 spawn_coin(map)
 while(True):
+    # clear the terminal
     os.system('cls' if os.name == 'nt' else 'clear')
     draw(map)
     print "points: {} \t level: {}".format(score, level)
     map = controller(getch(), map)
+    # if the score >= 500, move to the next level and reset the score
     if(score >= 500):
         level = level + 1
         map = load_map("map_{}.txt".format(level))
